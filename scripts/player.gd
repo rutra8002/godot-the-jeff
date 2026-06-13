8163160289ffb043f60ed654ae8c551c3879442f
rutra8002 <arturkummer08@gmail.com>
2026-06-13 18:24:00 +0200

    made movement better, added input stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,33 +1,47 @@
 extends CharacterBody2D
-
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+const GROUND_ACCEL = 10.0
+const GROUND_FRICTION = 10.0
+const AIR_ACCEL = 0.5
+const AIR_FRICTION = 0.01
+var sliding = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if Input.is_action_pressed("slide"):
+		sliding = true
+	else:
+		sliding = false
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("jump") and is_on_wall() and not is_on_floor():
+		var wall_normal = get_wall_normal()
+		velocity.x = wall_normal.x * SPEED
+		velocity.y = JUMP_VELOCITY
+	
+	var direction := Input.get_axis("move_left", "move_right")
+
+	if sliding:
+		direction = 0
+
+	if is_on_floor():
+		if not sliding:
+			velocity.x -= GROUND_FRICTION * velocity.x * delta
+		velocity.x += GROUND_ACCEL * direction * SPEED * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not sliding:
+			velocity.x -= AIR_FRICTION * velocity.x * delta
+		velocity.x += AIR_ACCEL * direction * SPEED * delta
 
 	move_and_slide()
-	
+
 func _draw():
-	# Draw a line representing your velocity vector
 	draw_line(Vector2.ZERO, velocity * 0.1, Color.GREEN, 2.0)
 
-func _process(delta):
-	# This forces the _draw() function to update every frame
+func _process(delta: float):
 	queue_redraw()
